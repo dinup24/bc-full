8b169c800720376f3daf18c83b48759b9fb18047
dinup24 <dinup24@gmail.com>
2020-11-13 00:10:42 +0530

    Update build.sh

--- scripts/web/build.sh
+++ scripts/web/build.sh
@@ -3,7 +3,7 @@
 echo "build web frontend"
 
 if [ $1 == "QUAY" ]; then
-   oc create -f $HERE/tekton-pipeline-run/catalog-run-quay.yaml
+   oc create -f $HERE/tekton-pipeline-run/web-run-quay.yaml
 else
-   oc create -f $HERE/tekton-pipeline-run/catalog-run-auto.yaml
+   oc create -f $HERE/tekton-pipeline-run/web-run-auto.yaml
 fi
